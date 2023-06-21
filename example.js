e6e886b2080c87a9f115a7abbc995f3b384a79ac
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2023-06-21 22:38:56 +0900

    login page & filter example

---
+++ example.js
@@ -0,0 +1,17 @@
+let product = {
+  refriger: [
+    {
+      size : 10,
+      color : 'white'
+    },
+    {
+
+    }
+  ],
+  toast : [],
+  heater : []
+}
+
+console.log(product)
+console.log(product.refriger);
+console.log(product.refriger[0]);
